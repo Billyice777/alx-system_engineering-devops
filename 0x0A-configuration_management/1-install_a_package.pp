1e00daef812e9f49c236caaafbbca50181ff1a5c
Billyice777 <uwemedimoekpewohp@gmail.com>
2023-11-24 18:51:35 -0800

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # Using puppet to install flask from pip3
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+package {
+  name: 'flask',
+  ensure: present,
+  provider: 'pip3',
+  version: '2.1.0',
 }
+
